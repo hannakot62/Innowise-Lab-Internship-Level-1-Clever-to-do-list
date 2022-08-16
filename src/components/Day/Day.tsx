4121486d86d6e9a3b15461f9f3495cb7702a0da3
HANNAKOT62 <2kotyagova6@gmail.com>
2022-08-16 22:09:56 +0300

    added days

---
+++ src/components/Day/Day.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Day.module.css'
+const Day = () => {
+    return (
+        <span>
+        <div className={style.mainContainer}>
+            <h1>17</h1>
+            <h4>today</h4>
+        </div>
+            </span>
+    );
+};
+
+export default Day;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import Task from "../components/Task/Task";
-
+import Day from "../components/Day/Day";
 const Main = (props:any) => {
     return (
         <div>
+            <div className={'days-container'}>
+                <Day/>
+                <Day/>
+                <Day/>
+                <Day/>
+                <Day/>
+            </div>
             <Task/>
             <Task/>
             <Task/>
